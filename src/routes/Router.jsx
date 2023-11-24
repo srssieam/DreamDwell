04770508c098aedd3ccc8a80525f7c29ddd757e2
srs-sieam <srssieam@gmail.com>
2023-11-24 07:41:02 +0600

    installed react router dome and created a router

---
+++ src/routes/Router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const Router = createBrowserRouter([
+    {
+        path:'/',
+        element:<div>Hello bai</div>
+    }
+])
+
+export default Router;
