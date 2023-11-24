ec3d9ed10d93156f800af291fbdd813a12e0a4cb
srs-sieam <srssieam@gmail.com>
2023-11-24 09:19:35 +0600

    created a simple navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import { Link, NavLink } from "react-router-dom";
+import './NavbarStyle.css'
+import logo from '../../assets/logo.png'
+import { FiUser } from "react-icons/fi";
+import { FaBars, FaXmark } from "react-icons/fa6";
+import { useState } from "react";
+
+
+const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
+    const navLinks = <>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/allProperty'>Property</NavLink></li>
+        <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
+    </>
+    return (
+        <div className=' bg-black'>
+            <div className='max-w-screen-xl mx-auto'>
+                <div className="navbar">
+                    <div className="navbar-start">
+                        <details onClick={()=>setMenuOpen(!menuOpen)} className="dropdown">
+                            <summary className="btn btn-ghost lg:hidden">
+                                {
+                                    menuOpen ? <FaXmark className="text-white text-xl"></FaXmark>
+                                    :<FaBars className="text-white text-xl"></FaBars> 
+                                }                                
+                            </summary>
+                            <ul className="p-2 shadow menu dropdown-content mt-3 z-[1] text-xl bg-black text-white rounded-box w-52">
+                                {navLinks}
+                            </ul>
+                        </details>
+                        <Link to='/' className="flex items-center">
+                            <img src={logo} alt="logo" className="w-12 h-12 lg:w-16 lg:h-16" />
+                            <div>
+                                <h3 className="lg:text-xl font-semibold text-[#54dd42]">Dream<span className="text-[#ffee00]">Dwell</span></h3>
+                                <p className="text-gray-300">Real estate</p>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className=" flex gap-6 px-2 font-handlee text-xl">
+                            {navLinks}
+                        </ul>
+                    </div>
+
+                    <div className="navbar-end">
+                        <li className=' list-none'><NavLink to='/login' className='flex items-center gap-2 mr-4'><FiUser className="text-[#ffee00]"></FiUser>Login</NavLink></li>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,10 @@
+import Navbar from "../Components/Navbar/Navbar";
 
 
 const Home = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <h1 className="text-5xl text-center">This is home page</h1>
         </div>
     );
