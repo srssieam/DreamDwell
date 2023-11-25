73f38f70517dccf610129b14449c3facd7c7f61c
srs-sieam <srssieam@gmail.com>
2023-11-25 19:18:43 +0600

    display all users in a tabular form

--- src/AdminDashboard/ManageUser.jsx
+++ src/AdminDashboard/ManageUser.jsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../hooks/useAxiosSecure";
+import { RiDeleteBin6Line } from "react-icons/ri";
+
 
 const ManageUser = () => {
     const axiosSecure = useAxiosSecure();
@@ -13,9 +15,45 @@ const ManageUser = () => {
     })
     
     return (
-        <div>
+        <div className="px-5">
             <h1 className="text-2xl lg:text-5xl text-center text-green-700 font-semibold italic lg:my-5">All Users</h1>
+            <p className="text-lg mb-4">Total User: {users.length}</p>
 
+            <div className="overflow-x-auto">
+                    <table className="table">
+                        <thead className="bg-black">
+                            <tr className="text-xl text-yellow-400 font-semibold ">
+                                <th></th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {
+                                users?.map((user, idx) => <tr key={users._id}>
+                                    <td>{idx + 1}</td>
+                                    <td>
+                                        {user.name}
+                                    </td>
+                                    <td>{user.email}</td>
+                                    <th className="flex gap-2">
+                                        <button className="rounded-md hover:text-yellow-500 p-2 text-green-600 border border-green-600 bg-black ">Make Admin</button>
+                                        <button className="rounded-md hover:text-yellow-500 p-2 text-green-600 border border-green-600 bg-black ">Make Agent</button>
+                                        <button className="rounded-md hover:text-yellow-500 p-2 text-green-600 border border-green-600 bg-black ">Mark as fraud</button>
+                                    </th>
+                                    <th>
+                                        <button className="p-2 rounded-md hover:bg-red-500 text-2xl text-white bg-red-700"><RiDeleteBin6Line></RiDeleteBin6Line></button>
+                                    </th>
+                                </tr>)
+                            }
+
+                        </tbody>
+
+                    </table>
+                </div>
         </div>
     );
 };
