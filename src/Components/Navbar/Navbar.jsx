39dccf67eee1cf54340fc99adb9ee5a22c59abe8
srs-sieam <srssieam@gmail.com>
2023-11-25 08:44:35 +0600

    logout button, user profile pic and user name has been displayed on navbar after login

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,22 +4,26 @@ import logo from '../../assets/logo.png'
 import { FiUser } from "react-icons/fi";
 import { FaBars, FaXmark } from "react-icons/fa6";
 import { useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import { IoMdLogOut } from "react-icons/io";
 
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false)
+    const { user } = useAuth();
     const navLinks = <>
         <li><NavLink to='/'>Home</NavLink></li>
         <li><NavLink to='/allProperty'>Property</NavLink></li>
         <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
+        <li className='text-[#54dd42] ml-4 md:hidden'>Logout |-</li>
     </>
     return (
         <div className='fixed z-20 bg-black w-full'>
             <div className='max-w-screen-xl mx-auto'>
                 <div className="navbar">
                     <div className="navbar-start">
-                        <details onClick={()=>setMenuOpen(!menuOpen)} className="dropdown">
-                            <summary className="btn btn-ghost lg:hidden">
+                        <details className="dropdown">
+                            <summary onClick={()=>setMenuOpen(!menuOpen)} className="btn btn-ghost lg:hidden">
                                 {
                                     menuOpen ? <FaXmark className="text-white text-xl"></FaXmark>
                                     :<FaBars className="text-white text-xl"></FaBars> 
@@ -44,7 +48,22 @@ const Navbar = () => {
                     </div>
 
                     <div className="navbar-end">
-                        <li className=' list-none'><NavLink to='/login' className='flex items-center gap-2 lg:text-lg mr-4 text-[#54dd42]'><FiUser></FiUser>Login</NavLink></li>
+                        {
+                            user ? < div className="flex flex-row">
+                                    <li className=' list-none hidden md:flex items-center gap-2 lg:text-lg text-[#54dd42] hover:text-[#ffee00] cursor-pointer mr-4'><IoMdLogOut></IoMdLogOut> Logout</li>
+                                    <div className="flex-col md:flex items-center justify-center">
+                                    {
+                                        user?.photoURL && <img src={user.photoURL} alt="" className="w-10 h-10 md:w-12 md:h-12 rounded-full mr-3"/>
+                                    }
+                                    {
+                                        user?.displayName && <p className="text-[#ffee00]">{user.displayName}</p>
+                                    }
+                                    </div>
+                                    </div>
+                            :
+                            <li className=' list-none'><NavLink to='/login' className='flex items-center gap-2 lg:text-lg mr-4 text-[#54dd42]'><FiUser></FiUser>Login</NavLink></li>
+                        }
+                        
                     </div>
                 </div>
             </div>
