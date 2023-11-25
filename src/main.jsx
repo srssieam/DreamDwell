1d44c3872fc117b701e4b56e4f44b49b7276127d
srs-sieam <srssieam@gmail.com>
2023-11-25 07:04:38 +0600

    Created Context API in AuthProvider and wraped RouterProvider in AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from './routes/Router'
+import AuthProvider from './Provider/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={Router}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={Router}></RouterProvider>
+    </AuthProvider>
   </React.StrictMode>
 )
