3dfa0fa37ea4cd284fef7d378763d8a1c281d2eb
srs-sieam <srssieam@gmail.com>
2023-11-27 06:26:24 +0600

    Delete a property

--- src/AdminDashboard/ManageProperties.jsx
+++ src/AdminDashboard/ManageProperties.jsx
@@ -1,9 +1,41 @@
 import { RiDeleteBin6Line } from "react-icons/ri";
 import useProperty from "../hooks/useProperty";
 import { MdLocationPin } from "react-icons/md";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../hooks/useAxiosSecure";
 
 const ManageProperties = () => {
-    const [properties] = useProperty();
+    const [properties, refetch] = useProperty();
+    const axiosSecure = useAxiosSecure();
+
+    const handleDeleteProperty = property => {
+        console.log('property to delete', property);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {    
+                axiosSecure.delete(`/properties/${property._id}`)
+                    .then(res =>{
+                        // console.log(res.data);
+                        refetch()
+                        if(res.data.deletedCount > 0){
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Property has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                })
+            }
+          }); 
+    }
+
     return (
         <div className="px-5">
             <h1 className="text-2xl lg:text-5xl text-center text-green-700 font-semibold italic lg:my-5">Manage Properties</h1>
@@ -38,7 +70,7 @@ const ManageProperties = () => {
                                     <button className="px-2 py-1 bg-red-700 hover:bg-red-600 rounded-md font-semibold text-white">Reject</button>
                                 </td>
                                 <td>
-                                    <button className="p-2 rounded-md hover:bg-red-500 text-2xl text-white bg-red-700"><RiDeleteBin6Line></RiDeleteBin6Line></button>
+                                    <button onClick={()=> handleDeleteProperty(property)} className="p-2 rounded-md hover:bg-red-500 text-2xl text-white bg-red-700"><RiDeleteBin6Line></RiDeleteBin6Line></button>
                                 </td>
                             </tr>)
                         }
