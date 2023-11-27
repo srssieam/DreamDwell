f48596d8e690cbf4fb87791bb49f3a3e8f2c20a5
srs-sieam <srssieam@gmail.com>
2023-11-27 07:38:45 +0600

    Verify and Reject Property

--- src/AdminDashboard/ManageProperties.jsx
+++ src/AdminDashboard/ManageProperties.jsx
@@ -1,6 +1,7 @@
 import { RiDeleteBin6Line } from "react-icons/ri";
 import useProperty from "../hooks/useProperty";
-import { MdLocationPin } from "react-icons/md";
+import { MdLocationPin, MdOutlineVerified } from "react-icons/md";
+import { RiForbid2Line } from "react-icons/ri";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../hooks/useAxiosSecure";
 
@@ -36,6 +37,64 @@ const ManageProperties = () => {
           }); 
     }
 
+    const handleVerify = property =>{
+        console.log('property to be verified', property);
+        Swal.fire({
+            title: "Verify property!",
+            text: "Are you sure you want to verify this property?",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonColor: "#00ad00",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes"
+          }).then((result) => {
+            if (result.isConfirmed) {    
+                axiosSecure.patch(`/properties/verify/${property._id}`)
+                    .then(res => {
+                        console.log(res.data);
+                        refetch();
+                        if(res.data.modifiedCount > 0){
+                            Swal.fire({
+                                title: "Property verified!",
+                                text: `${property.property_title} has been successfully verified.`,
+                                icon: "success",
+                                timer: 1500
+                            }); 
+                        }
+                     })
+            }
+          });
+    }
+
+    const handleReject = property =>{
+        console.log('property to be rejected', property);
+        Swal.fire({
+            title: "Reject property!",
+            text: "Are you sure you want to reject this property?",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonColor: "#00ad00",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes"
+          }).then((result) => {
+            if (result.isConfirmed) {    
+                axiosSecure.patch(`/properties/reject/${property._id}`)
+                    .then(res => {
+                        console.log(res.data);
+                        refetch();
+                        if(res.data.modifiedCount > 0){
+                            Swal.fire({
+                                title: "Property rejected!",
+                                text: `${property.property_title} has been successfully verified.`,
+                                icon: "success",
+                                timer: 1500
+                            }); 
+                        }
+                     })
+            }
+          });
+    }
+
     return (
         <div className="px-5">
             <h1 className="text-2xl lg:text-5xl text-center text-green-700 font-semibold italic lg:my-5">Manage Properties</h1>
@@ -65,10 +124,22 @@ const ManageProperties = () => {
                                     <p className="text-lg">{property.agent_name}</p>
                                     <p>{property.agent_email}</p>
                                 </td>
-                                <td className="flex flex-col justify-center items-center gap-2">
-                                    <button className="px-2 py-1 bg-green-700 hover:bg-green-600 rounded-md font-semibold text-white">Verify</button>
-                                    <button className="px-2 py-1 bg-red-700 hover:bg-red-600 rounded-md font-semibold text-white">Reject</button>
-                                </td>
+                                    {
+                                        property.verification_status === 'verified' ?
+                                        <td>
+                                            <span className="text-green-700 flex gap-1 justify-center items-center"><MdOutlineVerified className="text-xl"></MdOutlineVerified> verified</span>
+                                        </td>
+                                        : property.verification_status === 'rejected' ?
+                                        <td>
+                                            <span className="text-red-700 flex gap-1 justify-center items-center"><RiForbid2Line className="text-xl"></RiForbid2Line> rejected</span>
+                                        </td>
+                                        :
+                                        <td className="flex flex-col justify-center items-center gap-2">
+                                            <button onClick={()=> handleVerify(property)} className="px-2 py-1 bg-green-700 hover:bg-green-600 rounded-md font-semibold text-white">Verify</button>
+                                            <button onClick={()=> handleReject(property)} className="px-2 py-1 bg-red-700 hover:bg-red-600 rounded-md font-semibold text-white">Reject</button>
+                                        </td>       
+                                    }
+                                
                                 <td>
                                     <button onClick={()=> handleDeleteProperty(property)} className="p-2 rounded-md hover:bg-red-500 text-2xl text-white bg-red-700"><RiDeleteBin6Line></RiDeleteBin6Line></button>
                                 </td>
