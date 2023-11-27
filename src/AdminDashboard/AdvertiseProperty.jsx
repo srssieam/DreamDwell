d128612267e1da14987bbdc3efb810b61269020f
srs-sieam <srssieam@gmail.com>
2023-11-27 22:44:00 +0600

    remove advertise and conditionally disable button

--- src/AdminDashboard/AdvertiseProperty.jsx
+++ src/AdminDashboard/AdvertiseProperty.jsx
@@ -10,10 +10,10 @@ const AdvertiseProperty = () => {
     const axiosPublic = useAxiosPublic();
     const axiosSecure = useAxiosSecure();
 
-    const [advertisement] = useAdvertisement();
+    const [advertisement, refetch] = useAdvertisement();
     console.log(advertisement, 'from useAdvertise')
 
-    const { data: properties = [], refetch } = useQuery({
+    const { data: properties = [] } = useQuery({
         queryKey: ['verifiedProperty'],
         queryFn: async () => {
             const res = await axiosPublic.get('/allVerifiedProperties');
@@ -24,7 +24,7 @@ const AdvertiseProperty = () => {
     const handleAdvertise = property => {
         console.log('property to be advertised', property)
 
-        if(advertisement.length >= 6){
+        if (advertisement.length >= 6) {
             Swal.fire({
                 title: "Denied!",
                 text: "You can't advertise more then 6 properties.",
@@ -52,17 +52,17 @@ const AdvertiseProperty = () => {
     const handleRemove = property => {
         console.log('property to be removed', property)
         axiosSecure.delete(`/advertisement/${property._id}`)
-                    .then(res =>{
-                        console.log(res.data);
-                        refetch();
-                        if(res.data.deletedCount > 0){
-                            Swal.fire({
-                                title: "removed!",
-                                text: "Advertisement has been removed.",
-                                icon: "success"
-                            });
-                        }
-                })
+            .then(res => {
+                console.log(res.data);
+                refetch();
+                if (res.data.deletedCount > 0) {
+                    Swal.fire({
+                        title: "removed!",
+                        text: "Advertisement has been removed.",
+                        icon: "success"
+                    });
+                }
+            })
     }
 
 
@@ -71,6 +71,7 @@ const AdvertiseProperty = () => {
             <h1 className="text-2xl lg:text-5xl text-center text-green-700 font-semibold italic lg:my-5">Manage Properties</h1>
             <p className="text-red-600 font-semibold text-xl mb-4">You can advertise at most 6 properties</p>
             <p className="text-lg mb-4">Total verified Properties: {properties.length}</p>
+            <p className="text-lg mb-4">Total Advertisement: {advertisement.length}</p>
 
             <div className="border-2 border-green-700 p-4 grid md:grid-cols-2 gap-6">
                 {
@@ -88,12 +89,22 @@ const AdvertiseProperty = () => {
                                 </div>
                                 <p className='text-[#368a2b]'>Agent: {property.agent_name}</p>
                                 <p>$ ({property.price_range.lower_price} - {property.price_range.upper_price})</p>
+                                <p>{property._id}</p>
                             </div>
                         </div>
-                        <div className="text-center flex gap-4 justify-center mt-6">
-                            <button onClick={() => handleAdvertise(property)} className="text-white bg-green-700 hover:bg-green-600 rounded-md p-2">Advertise</button>
-                            <button onClick={() => handleRemove(property)} className="text-white bg-red-700 hover:bg-red-600 rounded-md p-2">Remove Advertise</button>
-                        </div>
+                        {
+                            advertisement.find(advertise => advertise._id == property._id) ?
+                                <div className="text-center flex gap-4 justify-center mt-6">
+                                    <button onClick={() => handleAdvertise(property)} className="text-gray-500 bg-gray-400 rounded-md p-2" disabled>Advertise</button>
+                                    <button onClick={() => handleRemove(property)} className="text-white bg-red-700 hover:bg-red-600 rounded-md p-2">Remove Advertise</button>
+                                </div>
+                                :
+                                <div className="text-center flex gap-4 justify-center mt-6">
+                                    <button onClick={() => handleAdvertise(property)} className="text-white bg-green-700 hover:bg-green-600 rounded-md p-2">Advertise</button>
+                                    <button onClick={() => handleRemove(property)} className="text-gray-500 bg-gray-400 rounded-md p-2" disabled>Remove Advertise</button>
+                                </div>
+                        }
+
                     </div>)
                 }
             </div>
