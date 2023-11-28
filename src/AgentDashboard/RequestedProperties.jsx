0ac71fb5c49ec8a7c177ca4099966ef081c36abc
srs-sieam <srssieam@gmail.com>
2023-11-28 22:02:45 +0600

    Accept and Reject offered property

--- src/AgentDashboard/RequestedProperties.jsx
+++ src/AgentDashboard/RequestedProperties.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../hooks/useAuth";
 import useAxiosSecure from "../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 
 const RequestedProperties = () => {
@@ -16,10 +17,62 @@ const RequestedProperties = () => {
 
     const handleAccept = property =>{
         console.log('property to be accepted', property)
+
+        Swal.fire({
+            title: "Accept offer!",
+            text: "Are you sure you want to accept this offer?",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonColor: "#00ad00",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes"
+          }).then((result) => {
+            if (result.isConfirmed) {    
+                axiosSecure.patch(`/offeredProperties/accept/${property._id}`)
+                    .then(res => {
+                        console.log(res.data);
+                        refetch();
+                        if(res.data.modifiedCount > 0){
+                            Swal.fire({
+                                title: "Offer accepted!",
+                                text: `the offer has been Accepted.`,
+                                icon: "success",
+                                timer: 1500
+                            }); 
+                        }
+                     })
+            }
+          });
     }
 
     const handleReject = property =>{
         console.log('property to be rejected', property)
+
+        Swal.fire({
+            title: "Reject offer!",
+            text: "Are you sure you want to reject this offer?",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonColor: "#00ad00",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes"
+          }).then((result) => {
+            if (result.isConfirmed) {    
+                axiosSecure.patch(`/offeredProperties/reject/${property._id}`)
+                    .then(res => {
+                        console.log(res.data);
+                        refetch();
+                        if(res.data.modifiedCount > 0){
+                            Swal.fire({
+                                title: "Offer rejected!",
+                                text: `the offer has been Rejected.`,
+                                icon: "success",
+                                timer: 1500
+                            }); 
+                        }
+                     })
+            }
+          });
     }
 
     return (
@@ -43,10 +96,18 @@ const RequestedProperties = () => {
                             <p><strong>Buyer:</strong> {property.buyer_name}</p>
                             <p><strong>Email:</strong> {property.buyer_email}</p>
                         </div>
-                        <div className="flex flex-col gap-3 justify-center ">
+                        {
+                            property.status === "accepted" ?
+                            <div className="flex justify-center"><p className="text-green-700 font-semibold"> Accepted</p></div>
+                            : property.status === "rejected" ?
+                            <div className="flex justify-center"><p className="text-red-700 font-semibold"> Rejected</p></div>
+                            :
+                            <div className="flex flex-col gap-3 justify-center ">
                             <button onClick={()=>handleAccept(property)} className="py-2 px-3 rounded bg-green-700 md:mx-7 font-playpen text-white font-semibold">Accept</button>
                             <button onClick={()=>handleReject(property)} className="py-2 px-3 rounded bg-red-700 md:mx-7 font-playpen text-white font-semibold">Reject</button>
                         </div>
+                        }
+                        
                     </div>
                 })
             }
